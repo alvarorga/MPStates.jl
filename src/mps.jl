2bbb7c9ab7d112b86a19a3b02f9ea7a3e7f2fd71
alvarorga <alvarorga@hotmail.com>
2019-04-20 14:36:20 +0200

    Fix types of MPS

--- src/mps.jl
+++ src/mps.jl
@@ -29,52 +29,52 @@ end
 Initialize an Mps.
 
 # Arguments:
-- `mps_type::Type`: type of the Mps.
+- `T::Type`: type of the Mps.
 - `L::Int`: Mps's length.
 - `name::String`: name of the Mps. Examples: "GHZ", "W", "product", "random",
     "full".
 - `d::Int=2`: physical dimension.
 """
-function init_mps(mps_type::Type, L::Int, name::String, d::Int=2)
-    A = Vector{Array{Float64, 3}}()
-    B = Vector{Array{Float64, 3}}()
+function init_mps(T::Type, L::Int, name::String, d::Int=2)
+    A = Vector{Array{T, 3}}()
+    B = Vector{Array{T, 3}}()
     d = 2
     D = ones(Int, L+1)
     # Build basis for constructing states.
     if name == "product"
-        M1 = zeros(mps_type, 1, d, 1)
-        M1[1, 1, 1] = one(mps_type)
+        M1 = zeros(T, 1, d, 1)
+        M1[1, 1, 1] = one(T)
         M = copy(M1)
         Mend = copy(M1)
     elseif name == "GHZ"
-        M1 = zeros(mps_type, 1, 2, 2)
-        M1[1, 1, 1] = one(mps_type)
-        M1[1, 2, 2] = one(mps_type)
-        M = zeros(mps_type, 2, 2, 2)
-        M[1, 1, 1] = one(mps_type)
-        M[2, 2, 2] = one(mps_type)
-        Mend = zeros(mps_type, 2, 2, 1)
-        Mend[1, 1, 1] = one(mps_type)
-        Mend[2, 2, 1] = one(mps_type)
+        M1 = zeros(T, 1, 2, 2)
+        M1[1, 1, 1] = one(T)
+        M1[1, 2, 2] = one(T)
+        M = zeros(T, 2, 2, 2)
+        M[1, 1, 1] = one(T)
+        M[2, 2, 2] = one(T)
+        Mend = zeros(T, 2, 2, 1)
+        Mend[1, 1, 1] = one(T)
+        Mend[2, 2, 1] = one(T)
     elseif name == "W"
-        M1 = zeros(mps_type, 1, 2, 2)
-        M1[1, 1, 1] = one(mps_type)
-        M1[1, 2, 2] = one(mps_type)
-        M = zeros(mps_type, 2, 2, 2)
-        M[1, 1, 1] = one(mps_type)
-        M[2, 1, 2] = one(mps_type)
-        M[1, 2, 2] = one(mps_type)
-        Mend = zeros(mps_type, 2, 2, 1)
-        Mend[2, 1, 1] = one(mps_type)
-        Mend[1, 2, 1] = one(mps_type)
+        M1 = zeros(T, 1, 2, 2)
+        M1[1, 1, 1] = one(T)
+        M1[1, 2, 2] = one(T)
+        M = zeros(T, 2, 2, 2)
+        M[1, 1, 1] = one(T)
+        M[2, 1, 2] = one(T)
+        M[1, 2, 2] = one(T)
+        Mend = zeros(T, 2, 2, 1)
+        Mend[2, 1, 1] = one(T)
+        Mend[1, 2, 1] = one(T)
     elseif name == "random"
-        M1 = rand(mps_type, 1, d, 2)
-        M = rand(mps_type, 2, d, 2)
-        Mend = rand(mps_type, 2, d, 1)
+        M1 = rand(T, 1, d, 2)
+        M = rand(T, 2, d, 2)
+        Mend = rand(T, 2, d, 1)
     elseif name == "full"
-        M1 = ones(mps_type, 1, d, 1)
-        M = ones(mps_type, 1, d, 1)
-        Mend = ones(mps_type, 1, d, 1)
+        M1 = ones(T, 1, d, 1)
+        M = ones(T, 1, d, 1)
+        Mend = ones(T, 1, d, 1)
     end
 
     # Join all tensors in arrays and make left- and right-canonical.
@@ -100,7 +100,7 @@ end
 
 Take a vector of tensors and make it left-canonical.
 """
-function make_left_canonical(A::Vector{Array{T, 3}}) where T
+function make_left_canonical(A::Vector{Array{T, 3}}) where T<:Number
     leftcan_A = Vector{Array{T, 3}}()
     Ai = A[1]
     for i=1:length(A)-1
@@ -122,7 +122,7 @@ end
 
 Take a vector of tensors and make it right-canonical.
 """
-function make_right_canonical(A::Vector{Array{T, 3}}) where T
+function make_right_canonical(A::Vector{Array{T, 3}}) where T<:Number
     rightcan_A = Vector{Array{T, 3}}()
     Ai = A[end]
     for i=length(A):-1:2
