e54244f1bf3e52cdb5e4aff2d408f6eea2601633
alvarorga <alvarorga@hotmail.com>
2019-04-22 00:17:27 +0200

    Compute expectation value of squared operator

--- src/mpo_operations.jl
+++ src/mpo_operations.jl
@@ -20,3 +20,23 @@ function expected(Op::Mpo{T}, psi::Mps{T}) where T
     end
     return L[1, 1, 1]
 end
+
+"""
+    expected2(Op::Mpo, psi::Mps)
+
+Compute expectation value of the squared operator < psi|Op^2|psi >.
+"""
+function expected2(Op::Mpo{T}, psi::Mps{T}) where T
+    L = ones(T, 1, 1, 1, 1)
+    for i=1:psi.L
+        Ai = psi.A[i]
+        Mi = Op.M[i]
+        @tensor begin
+            L1[a, s1, l2, l3, l4] := L[l1, l2, l3, l4]*Ai[l1, s1, a]
+            L2[a, b, s2, l3, l4] := L1[a, s1, l2, l3, l4]*Mi[l2, s1, s2, b]
+            L3[a, b, c, s3, l4] := L2[a, b, s2, l3, l4]*Mi[l3, s2, s3, c]
+            L[a, b, c, d] := L3[a, b, c, s3, l4]*conj(Ai[l4, s3, d])
+        end
+    end
+    return L[1, 1, 1]
+end
--- test/mpo_operations_tests.jl
+++ test/mpo_operations_tests.jl
@@ -10,3 +10,16 @@
     W = init_mps(Float64, L, "W")
     @test expected(H, W) ≈ 2t*(L-1)/L
 end
+
+@testset "expectation value of squared Mps" begin
+    L = 5
+    t = 1.5
+    U = 1.5
+    H = init_hubbard_mpo(L, t, U)
+    product = init_mps(Float64, L, "product")
+    @test MPStates.expected2(H, product) ≈ 0.
+    GHZ = init_mps(Float64, L, "GHZ")
+    @test MPStates.expected2(H, GHZ) ≈ U^2/2*(L-1)^2
+    W = init_mps(Float64, L, "W")
+    @test MPStates.expected2(H, W) ≈ t^2*(4L-6)/L
+end
