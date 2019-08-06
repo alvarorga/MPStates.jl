5fee81dfc59efa90fcf38766617b0c092847502f
alvarorga <alvarorga@hotmail.com>
2019-08-06 15:54:08 +0200

    Fix dmrg tests

--- test/dmrg_tests.jl
+++ test/dmrg_tests.jl
@@ -18,18 +18,20 @@ end
 
     # Max allowed bond dimension.
     m = 20
-    min_opts = MinimizeOpts(m, debug=0)
 
     # DMRG1.
     psi = init_mps(T, L, "W")
+    min_opts = MinimizeOpts(m, "DMRG1", debug=0)
     E1, var1 = minimize!(psi, H, min_opts)
 
     # DMRG2.
     psi = init_mps(T, L, "W")
+    min_opts = MinimizeOpts(m, "DMRG2", debug=0)
     E2, var2 = minimize!(psi, H, min_opts)
 
     # DMRG3S.
     psi = init_mps(T, L, "W")
+    min_opts = MinimizeOpts(m, "DMRG3S", debug=0)
     E3, var3 = minimize!(psi, H, min_opts)
 end
 end # @testset "Integration tests: variational/DMRG algorithms"
