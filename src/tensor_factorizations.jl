dc0aa2571f82d17636cce97488c2dbf0fe799434
alvarorga <alvarorga@hotmail.com>
2019-09-01 13:25:43 +0200

    Add SVD factorization of rank-3 and -4 tensors.

--- src/tensor_factorizations.jl
+++ src/tensor_factorizations.jl
@@ -1,12 +1,14 @@
 export factorize_qr,
-       factorize_lq
+       factorize_lq,
+       factorize_svd_right,
+       factorize_svd_left
 
 """
-    factorize_qr(M::Array{T, 3}) where T<:Number
+    factorize_qr(M::Array{<:Number, 3})
 
 QR factorization of M tensor.
 """
-function factorize_qr(M::Array{T, 3}) where T<:Number
+function factorize_qr(M::Array{<:Number, 3})
     m1, m2, m3 = size(M)
     M = reshape(M, m1*m2, m3)
     Q, R = qr(M)
@@ -16,15 +18,106 @@ function factorize_qr(M::Array{T, 3}) where T<:Number
 end
 
 """
-    factorize_lq(M::Array{T, 3}) where T<:Number
+    factorize_lq(M::Array{<:Number, 3})
 
 LQ factorization of M tensor.
 """
-function factorize_lq(M::Array{T, 3}) where T<:Number
+function factorize_lq(M::Array{<:Number, 3})
     m1, m2, m3 = size(M)
     M = reshape(M, m1, m2*m3)
     L, Q = lq(M)
     Q = Matrix(Q)
     Q = reshape(Q, size(Q, 1), m2, m3)
     return L, Q
+end
+
+"""
+    do_svd_with_options(M::AbstractMatrix{<:Number},
+                        cutoff::Float64,
+                        normalize_S::Bool)
+
+Do SVD with cutoff truncation and normalization of singular values.
+"""
+function do_svd_with_options(M::AbstractMatrix{<:Number},
+                             cutoff::Float64,
+                             normalize_S::Bool)
+    F = svd(M)
+    S = F.S
+    ix_cutoff = findlast(S .> cutoff)
+
+    S = Diagonal(S[1:ix_cutoff])
+    if normalize_S
+        S ./= norm(S)
+    end
+    U = F.U[:, 1:ix_cutoff]
+    Vt = F.Vt[1:ix_cutoff, :]
+    return U, S, Vt
+end
+
+"""
+    factorize_svd_right(M::Array{<:Number, 3};
+                        cutoff::Float64=1e-8,
+                        normalize_S::Bool=true)
+
+SVD factorization of M tensor reshapen to (M[1]*M[2], M[3]*1).
+"""
+function factorize_svd_right(M::Array{<:Number, 3};
+                             cutoff::Float64=1e-8,
+                             normalize_S::Bool=true)
+    U, SVt = factorize_svd_right(reshape(M, size(M)..., 1), cutoff, normalize_S)
+    SVt = reshape(SVt, size(SVt, 1), size(SVt, 2))
+    return U, SVt
+end
+
+"""
+    factorize_svd_left(M::Array{<:Number, 3};
+                       cutoff::Float64=1e-8,
+                       normalize_S::Bool=true)
+
+SVD factorization of M tensor reshapen to (1*M[1], M[2]*M[3]).
+"""
+function factorize_svd_left(M::Array{<:Number, 3};
+                            cutoff::Float64=1e-8,
+                            normalize_S::Bool=true)
+    US, Vt = factorize_svd_left(reshape(M, 1, size(M)...), cutoff, normalize_S)
+    US = reshape(US, size(US, 2), size(US, 3))
+    return US, Vt
+end
+
+"""
+    factorize_svd_right(M::Array{<:Number, 4};
+                        cutoff::Float64=1e-8,
+                        normalize_S::Bool=true)
+
+SVD factorization of M tensor reshapen to (M[1]*M[2], M[3]*M[4]).
+"""
+function factorize_svd_right(M::Array{<:Number, 4};
+                             cutoff::Float64=1e-8,
+                             normalize_S::Bool=true)
+    m1, m2, m3, m4 = size(M)
+    M = reshape(M, m1*m2, m3*m4)
+    U, S, Vt = do_svd_with_options(M, cutoff, normalize_S)
+    SVt = S*Vt
+    U = reshape(U, m1, m2, size(U, 2))
+    SVt = reshape(SVt, size(S, 1), m3, m4)
+    return U, SVt
+end
+
+"""
+    factorize_svd_right(M::Array{<:Number, 4}
+                        cutoff::Float64=1e-8,
+                        normalize_S::Bool=true)
+
+SVD factorization of M tensor reshapen to (M[1]*M[2], M[3]*M[4]).
+"""
+function factorize_svd_left(M::Array{<:Number, 4};
+                            cutoff::Float64=1e-8,
+                            normalize_S::Bool=true)
+    m1, m2, m3, m4 = size(M)
+    M = reshape(M, m1*m2, m3*m4)
+    U, S, Vt = do_svd_with_options(M, cutoff, normalize_S)
+    US = U*S
+    US = reshape(US, m1, m2, size(US, 2))
+    Vt = reshape(Vt, size(Vt, 1), m3, m4)
+    return US, Vt
 end
--- test/tensor_factorizations_tests.jl
+++ test/tensor_factorizations_tests.jl
@@ -1,6 +1,8 @@
-using Test, LinearAlgebra, MPStates
+using Test, Random, LinearAlgebra, MPStates
 
 @testset "Tensor factorizations" begin
+Random.seed!(0)
+
 @testset "QR decomposition" begin
     (m1, m2, m3) = (4, 5, 6)
     M = randn(m1, m2, m3)
@@ -34,5 +36,55 @@ end
 
     # Check that Q is unitary.
     @test Q*Q' ≈ Matrix(I, 4, 4)
+end
+
+@testset "SVD right decomposition" begin
+    (m1, m2, m3, m4) = (3, 4, 5, 3)
+    M = randn(m1, m2, m3, m4)
+
+    # Full factorization.
+    U, SVt = factorize_svd_right(M, cutoff=0., normalize_S=false)
+    @test size(U) == (3, 4, 12)
+    @test size(SVt) == (12, 5, 3)
+    rU = reshape(U, 12, 12)
+    @test rU'*rU ≈ Matrix(I, 12, 12)
+    rSVt = reshape(SVt, 12, 15)
+    @test rU*rSVt ≈ reshape(M, 12, 15)
+
+    # SVD with cutoff.
+    U, SVt = factorize_svd_right(M, cutoff=1., normalize_S=false)
+    @test size(U, 1) == 3
+    @test size(U, 2) == 4
+    @test size(U, 3) < 12
+    @test size(SVt, 2) == 5
+    @test size(SVt, 3) == 3
+    @test size(SVt, 1) < 12
+    rU = reshape(U, 12, size(U, 3))
+    @test rU'*rU ≈ Matrix(I, size(U, 3), size(U, 3))
+end
+
+@testset "SVD left decomposition" begin
+    (m1, m2, m3, m4) = (3, 4, 5, 3)
+    M = randn(m1, m2, m3, m4)
+
+    # Full factorization.
+    US, Vt = factorize_svd_left(M, cutoff=0., normalize_S=false)
+    @test size(US) == (3, 4, 12)
+    @test size(Vt) == (12, 5, 3)
+    rUS = reshape(US, 12, 12)
+    rVt = reshape(Vt, 12, 15)
+    @test rVt*rVt' ≈ Matrix(I, 12, 12)
+    @test rUS*rVt ≈ reshape(M, 12, 15)
+
+    # SVD with cutoff.
+    US, Vt = factorize_svd_left(M, cutoff=1., normalize_S=false)
+    @test size(US, 1) == 3
+    @test size(US, 2) == 4
+    @test size(US, 3) < 12
+    @test size(Vt, 2) == 5
+    @test size(Vt, 3) == 3
+    @test size(Vt, 1) < 12
+    rVt = reshape(Vt, size(Vt, 1), 15)
+    @test rVt*rVt' ≈ Matrix(I, size(Vt, 1), size(Vt, 1))
 end
 end # @testset "Tensor factorizations"
