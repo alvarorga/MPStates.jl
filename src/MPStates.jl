3c966f7006b1f168d0c516ac21a8adc509852d4c
alvarorga <alvarorga@hotmail.com>
2019-09-05 17:01:13 +0200

    Rename tensor_operations.jl to tensor_contractions.jl

--- src/MPStates.jl
+++ src/MPStates.jl
@@ -7,7 +7,7 @@ include("./mps_operations.jl")
 include("./mpo.jl")
 include("./mpo_operations.jl")
 include("./cache.jl")
-include("./tensor_operations.jl")
+include("./tensor_contractions.jl")
 include("./tensor_factorizations.jl")
 include("./dmrg.jl")
 
