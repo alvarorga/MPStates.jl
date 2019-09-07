f244e25ec77dd34ef02eb3c8c489f5d2a13e938c
alvarorga <alvarorga@hotmail.com>
2019-09-07 11:58:55 +0200

    Reorder include statements

--- src/MPStates.jl
+++ src/MPStates.jl
@@ -2,14 +2,14 @@ module MPStates
 
 using LinearAlgebra, TensorOperations, Random, HDF5, Printf, Arpack
 
+include("./cache.jl")
+include("./tensor_contractions.jl")
+include("./tensor_factorizations.jl")
 include("./mps.jl")
 include("./mps_operations.jl")
 include("./mpo.jl")
 include("./mpo_operations.jl")
-include("./cache.jl")
-include("./tensor_contractions.jl")
-include("./tensor_factorizations.jl")
-include("./dmrg.jl")
 include("./dmrg_opts.jl")
+include("./dmrg.jl")
 
 end # module MPStates
